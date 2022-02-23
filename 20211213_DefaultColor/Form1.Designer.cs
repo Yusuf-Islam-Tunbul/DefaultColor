e78de0c041e0e21c03e27de54ea8ab3938c06958
Yusuf-Islam-Tunbul <50180280+Yusuf-Islam-Tunbul@users.noreply.github.com>
2022-02-23 21:23:01 +0300

    Proje dosyası ekle.

---
+++ 20211213_DefaultColor/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace _20211213_DefaultColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.change_color_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // change_color_button
+            // 
+            this.change_color_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.change_color_button.Location = new System.Drawing.Point(68, 45);
+            this.change_color_button.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.change_color_button.Name = "change_color_button";
+            this.change_color_button.Size = new System.Drawing.Size(231, 123);
+            this.change_color_button.TabIndex = 0;
+            this.change_color_button.Text = "RENK DEĞİŞTİR";
+            this.change_color_button.UseVisualStyleBackColor = true;
+            this.change_color_button.Click += new System.EventHandler(this.change_color_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 216);
+            this.Controls.Add(this.change_color_button);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button change_color_button;
+    }
+}
+
---
+++ 20211213_DefaultColor/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211213_DefaultColor
+{
+    public partial class Form1 : Form
+    {
+        Color[] button_colors=new Color[2];
+        bool color_switch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button_colors[0] = change_color_button.BackColor;
+            button_colors[1] = Color.Green;
+        }
+
+        private void change_color_button_Click(object sender, EventArgs e)
+        {
+            ChangeColorButtonMethod();
+        }
+
+        void ChangeColorButtonMethod()
+        {
+            if (color_switch)
+            {
+                ChangeButtonColor(button_colors[0]);
+            }
+
+            else
+            {
+                ChangeButtonColor(button_colors[1]);
+            }
+
+            color_switch = !color_switch;
+        }
+
+        void ChangeButtonColor(Color color)
+        {
+            change_color_button.BackColor = color;
+        }
+    }
+}
